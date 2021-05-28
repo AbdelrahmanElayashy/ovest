f1acdb9f34b23412f697a99a5c421bd4642c403c
omar elayashy <omartalal95@gmail.com>
2021-05-28 12:32:46 +0200

    refactoring falldetaction algo

--- ovest/acceleration/falldetectionalgo.py
+++ ovest/acceleration/falldetectionalgo.py
@@ -1,82 +1,84 @@
-# from ovest.acceleration import AccModule
-# import math
-# import time
+import math
+import time
 
 
-# fall = False
-# trigger1 = False
-# trigger2 = False
-# trigger3 = False
-# trigger1count = 0
-# trigger2count = 0
-# trigger3count = 0
+class FallDetection:
 
-# while True:
-#     sig = AccModule.get_signals()
+    def __init__(self):
+        self.fall = False
+        self.trigger1 = False
+        self.trigger2 = False
+        self.trigger3 = False
+        self.trigger1count = 0
+        self.trigger2count = 0
+        self.trigger3count = 0
 
-#     am = math.sqrt(math.pow(sig[0], 2) +
-#                    math.pow(sig[1], 2) + math.pow(sig[2], 2))
+    def fall_detection(self, sig):
+        am = math.sqrt(math.pow(sig[0], 2) +
+                       math.pow(sig[1], 2) + math.pow(sig[2], 2))
 
-#     if trigger3 == True:
-#         trigger3count += 1
-#         if trigger3count >= 10:
+        if self.trigger3 == True:
+            self.trigger3count += 1
+            if self.trigger3count >= 10:
 
-#             angleChange = math.sqrt(
-#                 math.pow(sig[3], 2) + math.pow(sig[4], 2) + math.pow(sig[5], 2))
-#             print("The Angle Change in Trigger 3 is {}".format(angleChange))
+                angleChange = math.sqrt(
+                    math.pow(sig[3], 2) + math.pow(sig[4], 2) + math.pow(sig[5], 2))
+                print("The Angle Change in Trigger 3 is {}".format(angleChange))
 
-#             if (angleChange >= 0 and angleChange <= 10):
-#                 Fall = True
-#                 trigger3 = False
-#                 trigger3count = 0
-#                 print("The Angle Change in case of Fall")
-#                 print("Fall detected")
-#                 exit()
-#             else:
-#                 trigger3 = False
-#                 trigger3count = 0
-#                 print("Trigger3 Deactivated when Fall is not detected")
-#     if fall == True:
-#         print("Anet at fall={}".format(am))
-#         print("Gnet at fall={}".format(angleChange))
-#         print("Fall Detected")
+                if (angleChange >= 0 and angleChange <= 10):
+                    self.fall = True
+                    self.trigger3 = False
+                    self.trigger3count = 0
+                    print("The Angle Change in case of Fall")
+                    print("Fall detected")
+                    #exit()
+                else:
+                    self.trigger3 = False
+                    self.trigger3count = 0
+                    print("Trigger3 Deactivated when Fall is not detected")
+        if self.fall == True:
+            print("Anet at fall={}".format(am))
+            print("Gnet at fall={}".format(angleChange))
+            print("Fall Detected")
 
-#         # Send Alert to caregiver: make sure that don't exceed 1 min
-#         fall = False
+            # Send Alert to caregiver: make sure that don't exceed 1 min
+            self.fall = False
+            return not self.fall
 
-#     if trigger2count >= 6:
-#         trigger2 = False
-#         trigger2count = 0
-#         print("Trigger 2 Deactivated: ALlow 0.5s for Orientation Change")
+        if self.trigger2count >= 6:
+            self.trigger2 = False
+            self.trigger2count = 0
+            print("Trigger 2 Deactivated: ALlow 0.5s for Orientation Change")
 
-#     if trigger1count >= 6:
-#         trigger1 = False
-#         trigger1count = 0
-#         print("Trigger 1 Deactivated: ALlow 0.5s for AM to break upper Threshold")
+        if self.trigger1count >= 6:
+            self.trigger1 = False
+            self.trigger1count = 0
+            print("Trigger 1 Deactivated: ALlow 0.5s for AM to break upper Threshold")
 
-#     if trigger2 == True:
-#         trigger2count += 1
-#         angleChange = math.sqrt(
-#             math.pow(sig[3], 2) + math.pow(sig[4], 2) + math.pow(sig[5], 2))
-#         print("The angle Change in Trigger 2 is {}".format(angleChange))
+        if self.trigger2 == True:
+            self.trigger2count += 1
+            angleChange = math.sqrt(
+                math.pow(sig[3], 2) + math.pow(sig[4], 2) + math.pow(sig[5], 2))
+            print("The angle Change in Trigger 2 is {}".format(angleChange))
 
-#         if (angleChange >= 30 and angleChange <= 400):
-#             trigger3 = True
-#             trigger2 = False
-#             trigger2count = 0
-#             print("The angle Change in Trigger 3 is {}".format(angleChange))
-#             print("Trigger 3 Activated")
+            if (angleChange >= 30 and angleChange <= 400):
+                self.trigger3 = True
+                self.trigger2 = False
+                self.trigger2count = 0
+                print("The angle Change in Trigger 3 is {}".format(angleChange))
+                print("Trigger 3 Activated")
 
-#     if trigger1 == True:
-#         trigger1count += 1
-#         if am >= 1.2:
-#             trigger2 = True
-#             print("Trigger 2 Activated: AM breaks upper threshold")
-#             trigger1 = False
-#             trigger1count = 0
+        if self.trigger1 == True:
+            self.trigger1count += 1
+            if am >= 1.2:
+                self.trigger2 = True
+                print("Trigger 2 Activated: AM breaks upper threshold")
+                self.trigger1 = False
+                self.trigger1count = 0
 
-#     if (am <= 2 and trigger2 == False):
-#         trigger1 = True
-#         print("Trigger 1 Activated: AM breaks lower threshold 0.4g")
+        if (am <= 2 and self.trigger2 == False):
+            self.trigger1 = True
+            print("Trigger 1 Activated: AM breaks lower threshold 0.4g")
 
-#     time.sleep(0.05)
+        #time.sleep(0.05)
+        return self.fall
--- ovest/acceleration/run.py
+++ ovest/acceleration/run.py
@@ -1,15 +1,21 @@
 from ovest.acceleration import AccModule
 from ovest.acceleration import falldetectionalgo
+import time
 
 is_fall = False
 
 
 def run(bus, dev_addr):
     mpu = AccModule.Mpu6050(bus, dev_addr)
+    fall_obj = falldetectionalgo.FallDetection()
     global is_fall
     while True:
         sig = mpu.get_signals()
-        #is_fall = falldetection(sig)
+        is_fall = fall_obj.fall_detection(sig)
+        time.sleep(0.05)
+        if is_fall:
+            for i in range(1000):
+                print("###############FALL DETECTED#######################")
 
 
 def get_is_fall():
